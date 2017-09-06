ff4097539f9fb72f81f2e8969967c420e19dfdce
Josef Schugt <penpendede@mail.ru>
2017-09-06 22:51:41 +0200

    clustering instead of vectortiles

--- scripts/copyFilesToWebDirectory.js
+++ scripts/copyFilesToWebDirectory.js
@@ -1,14 +1,25 @@
 const fs = require('fs-extra')
 
 // cleanup
-fs.removeSync('../website')
+fs.removeSync('./website')
 
 // copy leaflet
 fs.copySync('./node_modules/leaflet/dist/leaflet.css', './website/css/leaflet.css')
 fs.copySync('./node_modules/leaflet/dist/leaflet.js', './website/js/leaflet.js')
 fs.copySync('./node_modules/leaflet/dist/images', './website/css/images')
 
-fs.copySync('./node_modules/leaflet.vectorgrid/dist/Leaflet.VectorGrid.bundled.min.js', './website/js/Leaflet.VectorGrid.js')
+fs.copySync(
+  './node_modules/leaflet.markercluster/dist/MarkerCluster.css',
+  './website/css/MarkerCluster.css'
+)
+fs.copySync(
+  './node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css',
+  './website/css/MarkerCluster.Default.css'
+)
+fs.copySync(
+  './node_modules/leaflet.markercluster/dist/leaflet.markercluster.js',
+  './website/js/leaflet.markercluster.js'
+)
 
 // copy page source
 fs.copySync('./src', './website')
