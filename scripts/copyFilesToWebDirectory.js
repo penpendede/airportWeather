3d8d8fd85920ed9a5ec39056d0e6a675d6f2cd74
Josef Schugt <penpendede@mail.ru>
2017-09-13 14:31:21 +0200

    copy tasks configured in conf/copyTasks.json, dependency on leaflet-easybutton added

--- scripts/copyFilesToWebDirectory.js
+++ scripts/copyFilesToWebDirectory.js
@@ -1,38 +1,8 @@
 const fs = require('fs-extra')
+const copyTasks = require('../conf/copyTasks')
 
-// cleanup
 fs.removeSync('./website')
 
-// copy leaflet
-fs.copySync('./node_modules/leaflet/dist/leaflet.css', './website/css/leaflet.css')
-fs.copySync('./node_modules/leaflet/dist/leaflet.js', './website/js/leaflet.js')
-fs.copySync('./node_modules/leaflet/dist/images', './website/css/images')
-
-// copy leaflet.markercluster
-fs.copySync(
-  './node_modules/leaflet.markercluster/dist/MarkerCluster.css',
-  './website/css/MarkerCluster.css'
-)
-fs.copySync(
-  './node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css',
-  './website/css/MarkerCluster.Default.css'
-)
-fs.copySync(
-  './node_modules/leaflet.markercluster/dist/leaflet.markercluster.js',
-  './website/js/leaflet.markercluster.js'
-)
-
-// copy page source
-fs.copySync('./src', './website')
-
-// copy images
-fs.copySync('./images', './website/images')
-
-// copy css
-fs.copySync('./css', './website/css')
-
-// copy parser
-fs.copySync('./lib', './website/js')
-
-// copy airport list
-fs.copySync('./data/supportedAirports.geojson', './website/data/airports.geojson')
+for (let src in copyTasks) {
+  fs.copy(src, copyTasks[src])
+}
