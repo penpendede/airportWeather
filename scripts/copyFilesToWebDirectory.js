a4ffab672d702060c7e01d1e15b43827c44d9bfa
Josef Schugt <penpendede@mail.ru>
2020-09-12 16:57:26 +0200

    fixed copyFilesToWebDirectory.js

--- scripts/copyFilesToWebDirectory.js
+++ scripts/copyFilesToWebDirectory.js
@@ -2,7 +2,12 @@ const fs = require('fs-extra')
 const copyTasks = require('../conf/copyTasks')
 
 fs.removeSync('./website')
+fs.mkdirSync('./website')
+fs.mkdirSync('./website/css')
+fs.mkdirSync('./website/js')
 
 for (let src in copyTasks) {
-  fs.copy(src, './website/' + copyTasks[src])
+  fs.copy(src, './website/' + copyTasks[src], {
+    overwrite: true
+  })
 }
